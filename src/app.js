a556e5012e1c14607450a4a7b18a4b6dc07ff952
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2021-12-26 21:48:01 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const http = require("http");
+
 const app = express();
 
 const server = http.createServer(app);
@@ -9,34 +10,21 @@ const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 1337;
 
-/**
- * Configuration du moteur de template
- */
-
 app.set("view engine", "pug");
 app.set("views", "./src/views");
 
-app.locals.pretty = true; // Utile si on veut debugger le code HTML généré coté client
+app.locals.pretty = true;
 
-require("./chat-serveur")(io);
-
-/**
- * Middlewares (fonctions intermediaires) dee l'application Express
- */
+require('./chat-serveur')(io);
 
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(express.static("./src/static"));
 
-/**
- * Route de l'application Express
- */
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Route qui va écouter sur http://localhost:1337/chat
 app.get("/chat", (req, res) => {
   if (!req.query.pseudo) {
     return res.redirect("/");
@@ -47,7 +35,6 @@ app.get("/chat", (req, res) => {
   res.render("chat", { pseudo });
 });
 
-// L'application Express va écouter et repondre sur le port réseau n°1337
 server.listen(PORT, function () {
   console.log(`Le serveur écoute sur http://localhost:${PORT}`);
 });
